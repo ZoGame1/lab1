7af522314415c75748ab8a370811d79b90737026
ZoGame1 <162279207+ZoGame1@users.noreply.github.com>
2024-05-09 00:18:48 +0700

    2ff

---
+++ Mapslab1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+	public class Mapslab1
+	{
+	string ID;
+	string Name;
+
+	private static string InGame = "Lien Minh Huyen Thoai";
+
+	public Mapslab1(string iD, string name)
+	{
+		ID = iD;
+		Name = name;
+	}
+	public string showData()
+	{
+		return "ID: "+ID + " | Name: " + Name + " | IN Game" + InGame;
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+	public static class UserData
+	{
+		public static string ID;
+		public static string UserName;
+		public static float Health;
+		public static int Point;
+
+		static UserData()
+		{
+			ID = "2203311";
+			UserName = "Quoc phan";
+			Health = 110f;
+			Point = 300;
+		}
+		public static void Main(string[] args)
+		{
+			System.Console.WriteLine("ID " + UserData.ID);
+			System.Console.WriteLine("Name " + UserData.UserName);
+			System.Console.WriteLine("Health " + UserData.Health);
+			System.Console.WriteLine("Point " + UserData.Point);
+
+		}
+	}
+
