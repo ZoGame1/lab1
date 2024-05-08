e965cc6d6c629a83fd78972363eb21509ccdfa1d
ZoGame1 <162279207+ZoGame1@users.noreply.github.com>
2024-05-09 00:04:11 +0700

    2f

---
+++ C#1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		Mapslab1 Map = new Mapslab1("PD11057", "MOT CHO TAT CA");
+		Console.WriteLine(Map.showData());
+		}
+	}
+
